d6a188314cb185c3ace770015885232722c04ab2
Argha <email2argha@gmail.com>
2020-11-20 13:59:11 +0530

    Socket.io version downgraded to fix CORS issue

--- README.md
+++ README.md
@@ -19,8 +19,16 @@ git clone git@github.com:argha0977/node-express-realtime-api.git
 ## Dependencies
 
 ```js
-npm install cors winston socket.io --save
+npm install cors winston socket.io@2.2.0 --save
 npm install
+
+```
+* The latest version of socket.io(3.0.3) has some cors related issues.
+
+## Client App Dependencies
+* The client angular or Ionic app should use socket.io-client@2.2.0. So use the following command to install,
+```js
+npm install socket.io-client@2.2.0
 ```
 
 ## Run
@@ -43,5 +51,5 @@ pm2 stop NODE_EXPRES_REALTIME_API
 ## URL
 
 ```js
-http://localhost:4013/api\
+http://localhost:4013/api/
 ```
--- routes/notificationsocket.js
+++ routes/notificationsocket.js
@@ -8,10 +8,11 @@ const logger = require('../config/logger');
 var notificationsocket = function (io) {
     io.on('connection', (socket) => {
         logger.logInfo('Client Connected');
-        socket.on('newNotification', function (obj, callback) {
+        socket.on('newNotification', function (obj) {
             console.log(obj);
-            callback(obj);
-            socket.broadcast.emit('newNotification', obj);
+            io.emit('newNotification', obj);// Send to all including sender
+            //socket.emit('newNotification', obj);// Send to sender only
+            //socket.broadcast.emit('newNotification', obj);// Send to all except sender
         })
     });
 
